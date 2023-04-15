e8840d5ac86b2914017ca6aa0d620e308b1bb3c3
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2023-04-15 05:21:38 +0200

    add pending payments component

---
+++ InvoiceSystem/src/app/pending-payments/pending-payments.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pending-payments',
+  templateUrl: './pending-payments.component.html',
+  styleUrls: ['./pending-payments.component.css']
+})
+export class PendingPaymentsComponent {
+
+}
