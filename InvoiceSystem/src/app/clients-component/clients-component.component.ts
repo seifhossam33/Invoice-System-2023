7672af8cf396d6c9f3c265469d212065f746b081
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2023-04-17 01:54:09 +0200

    create clients table

---
+++ InvoiceSystem/src/app/clients-component/clients-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clients-component',
+  templateUrl: './clients-component.component.html',
+  styleUrls: ['./clients-component.component.css'],
+})
+export class ClientsComponentComponent {
+  showCheckboxColumn: boolean = false;
+  billsTable: string = 'clientsTable';
+}
