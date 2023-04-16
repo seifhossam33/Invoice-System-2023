598fdd3c3b3da636600d4939a27f4129ed8408a6
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2023-04-17 00:38:13 +0200

    update userProfile styling

--- InvoiceSystem/src/app/user-profile/user-profile.component.ts
+++ InvoiceSystem/src/app/user-profile/user-profile.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+  styleUrls: ['./user-profile.component.css'],
 })
 export class UserProfileComponent {
+  isReadOnly: boolean = true;
 
+  toggleReadOnly() {
+    this.isReadOnly = !this.isReadOnly;
+  }
 }
