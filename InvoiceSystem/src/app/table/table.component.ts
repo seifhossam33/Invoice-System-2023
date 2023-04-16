d1e0c60b9e1de6e13e47dfe564c52d9b3ee0ca48
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2023-04-16 22:53:01 +0200

    finish tableComponent

--- InvoiceSystem/src/app/table/table.component.ts
+++ InvoiceSystem/src/app/table/table.component.ts
@@ -1,14 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { DataService } from '../data.service';
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css'],
 })
-export class TableComponent {
-  selectedOption: string = '';
-  @Input() showCheckboxColumn: boolean = false;
-
+export class TableComponent implements OnInit {
   columns: string[] = [
     'Service',
     'Start date',
@@ -17,10 +14,51 @@ export class TableComponent {
     'Total units used',
     'Invoice Amount',
     'Status',
+    'isSelected',
   ];
+  selectedAll: boolean = false;
+  @Input() showCheckboxColumn: boolean = false;
+
+  selectedOption: string = '';
+  data: any[] = [];
+  filteredData: any[] = [];
   tableData: any[] = [];
-  constructor(private dataService: DataService) {
-    this.tableData = this.dataService.getTableData();
-    console.log(this.tableData);
+  constructor(private dataService: DataService) {}
+  ngOnInit() {
+    this.dataService.data$.subscribe((data) => {
+      this.data = data;
+      this.filterTableData();
+    });
+
+    this.dataService.selectedOption$.subscribe((option) => {
+      this.selectedOption = option;
+      this.filterTableData();
+    });
+  }
+  filterTableData() {
+    console.log(this.selectedOption);
+    if (this.selectedOption === '1') {
+      // electricity
+      this.tableData = this.data.filter(
+        (item) => item.Service === 'Electricity'
+      );
+    } else if (this.selectedOption === '2') {
+      // water
+      this.tableData = this.data.filter((item) => item.Service === 'Water');
+    } else if (this.selectedOption === '3') {
+      // telephone
+      this.tableData = this.data.filter((item) => item.Service === 'Telephone');
+    } else {
+      this.tableData = this.data;
+    }
+  }
+
+  selectAllCheckBoxes() {
+    for (let row of this.tableData) {
+      row.isSelected = this.selectedAll;
+    }
+  }
+  onChangeCheckbox() {
+    this.selectedAll = this.tableData.every((item) => item.selected);
   }
 }
