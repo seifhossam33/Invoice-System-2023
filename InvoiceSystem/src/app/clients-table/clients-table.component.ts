7164dbbddc8403620af355772733c41796c56922
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2023-04-30 01:29:37 +0300

    display users

--- InvoiceSystem/src/app/clients-table/clients-table.component.ts
+++ InvoiceSystem/src/app/clients-table/clients-table.component.ts
@@ -2,7 +2,9 @@ import { Component, Input } from '@angular/core';
 import { DataService } from '../services/data.service';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
-import { TableDataService } from '../services/table-data.service';
+import { FirebaseService } from '../services/auth.service';
+import { Client } from '../interfaces/client.interface';
+
 @Component({
   selector: 'app-clients-table',
   templateUrl: './clients-table.component.html',
@@ -12,14 +14,13 @@ export class ClientsTableComponent {
   constructor(
     private dataService: DataService,
     private router: Router,
-    private tableService: TableDataService
+    private clientService: FirebaseService
   ) {}
 
   columns: any[] = [];
-  data: any[] = [];
   tableData: any[] = [];
-  tableObservable!: Subscription;
   ngOnInit() {
+    this.clientService.getUsers().subscribe((items) => (this.tableData = items));
     this.columns = this.dataService.getTableHeaders('clientsTable');
   }
   onShowInvoices(clientId: number) {
