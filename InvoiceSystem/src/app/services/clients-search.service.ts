783e745766cddbcea6406069ef157d326a0f57ff
seifhossam33 <84685631+seifhossam33@users.noreply.github.com>
2023-05-01 21:09:43 +0300

    Finishing Search service with all letter cases

---
+++ InvoiceSystem/src/app/services/clients-search.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+  private searchValue = new BehaviorSubject<string>('');
+  currentSearchValue = this.searchValue.asObservable();
+
+  updateSearchValue(value: string) {
+    console.log(value);
+    console.log(this.searchValue);
+    this.searchValue.next(value);
+    console.log(this.searchValue);
+  }
+}
