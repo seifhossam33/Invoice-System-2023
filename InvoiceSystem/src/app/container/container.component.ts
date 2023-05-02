bed7f1622e4c952e82b4166ff837b268f199af35
salma tarek <62105437+salmaatarekk@users.noreply.github.com>
2023-05-02 17:58:15 +0300

    fix default page routing

--- InvoiceSystem/src/app/container/container.component.ts
+++ InvoiceSystem/src/app/container/container.component.ts
@@ -1,24 +1,22 @@
-import { Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
-
 
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
-  styleUrls: ['./container.component.css']
+  styleUrls: ['./container.component.css'],
 })
-export class ContainerComponent implements OnInit{
+export class ContainerComponent implements OnInit {
   isLoginPage: boolean = false;
   isSignUp: boolean = false;
 
-  constructor(private router: Router){ }
+  constructor(private router: Router) {}
   ngOnInit() {
-    this.router.events.subscribe(event => {
-      if(event instanceof NavigationEnd){
-        this.isLoginPage = (event.url ==='/login');
-        this.isSignUp = (event.url ==='/signup');
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isLoginPage = event.url === '/login' || event.url === '/';
+        this.isSignUp = event.url === '/signup';
       }
     });
   }
-
 }
