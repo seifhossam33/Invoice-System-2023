c4ba303f1fb474883d76939a9d452fdd6aeae18b
Nouran Elbagory <i.nouran@yahoo.com>
2023-05-02 17:00:30 +0300

    login fixed

--- InvoiceSystem/src/app/login/login.component.ts
+++ InvoiceSystem/src/app/login/login.component.ts
@@ -3,7 +3,7 @@ import { NgForm } from '@angular/forms';
 import { FirebaseService } from 'src/app/services/auth.service';
 import { Client } from '../interfaces/client.interface';
 import { Router } from '@angular/router';
-import { ToastService } from '../services/toast.service';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -13,7 +13,7 @@ export class LoginComponent {
   constructor(
     private authService: FirebaseService,
     private router: Router,
-    private toast: ToastService
+    private toast: ToastrService
   ) {}
 
   user = {
@@ -21,19 +21,17 @@ export class LoginComponent {
     password: '',
   };
 
-  onSubmit(form: NgForm) {
+ async onSubmit(form: NgForm) {
     let user: Client = form.value;
     if (user.email && user.password) {
-      this.authService
-        .login(user.email, user.password)
-        .then(() => {
-          // Login successful, navigate to the user profile page
-          this.router.navigate(['/userProfile']);
-        })
-        .catch((error) => {
-          // Login failed, display an error message or handle the error in some other way
-          console.log("there is an error", error);
-        });
+      const success = await this.authService.login(user.email, user.password)
+      if (success) {
+        this.router.navigate(['/userProfile']);
+        this.toast.success("Login Success")
+      } else {
+        this.toast.error("Login Failed");
+      }
     }
   }
+
 }
