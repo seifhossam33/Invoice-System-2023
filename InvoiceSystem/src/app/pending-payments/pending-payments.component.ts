2ea65f2ec85ec3a420ca8faebca30e0db65dd51e
noranzaki <noranzaki2012@gmail.com>
2023-05-02 20:06:34 +0200

    feature 3

--- InvoiceSystem/src/app/pending-payments/pending-payments.component.ts
+++ InvoiceSystem/src/app/pending-payments/pending-payments.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
+import { BillsToPayService } from '../services/bills-to-pay.service';
+import { Bill } from '../interfaces/bill';
 
 @Component({
   selector: 'app-pending-payments',
@@ -14,11 +16,28 @@ export class PendingPaymentsComponent {
 
   // todo implement on pay method
   onPay() {
+    const selectedBillsToPay = this.billsToPayService.selectedBillsToPay; 
+    console.log(selectedBillsToPay);
+    const currentDate = new Date();
+    const fixedAmount = 50; 
+
+    for (const bill of selectedBillsToPay) {
+      const lastDate = new Date(bill['Last date']);
+      if (lastDate < currentDate) {
+        bill['Invoice Amount'] += fixedAmount; 
+      }
+    }
+
     this.router.navigate(['/pay']);
   }
-  constructor(private dataService: DataService, private router: Router) {}
+  constructor(
+    private dataService: DataService,
+    private router: Router,
+    private billsToPayService: BillsToPayService
+     ) {}
   ngOnInit() {
     this.dataService.resetSelectedOption();
+    
   }
 
   // todo displays bills with postpaid status get client id from routing
