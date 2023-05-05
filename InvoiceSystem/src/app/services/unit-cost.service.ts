3dd88b81207f0c29155b5adc9f8f7a4422d40d39
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2023-05-05 05:11:29 +0300

    display the current unit cost of services

--- InvoiceSystem/src/app/services/unit-cost.service.ts
+++ InvoiceSystem/src/app/services/unit-cost.service.ts
@@ -3,40 +3,44 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UnitCostService {
-
-  constructor(private angularFS: AngularFirestore) { }
-
+  constructor(private angularFS: AngularFirestore) {}
+  unitCostOfWater: string = '';
+  unitCostOfElectricity: string = '';
   updateUnitCost(newCost: number, type: string) {
-    if (type == "electricity") {
+    if (type == 'electricity') {
       const docId = 'VaTEFMevPbnyWUb8O1XR';
-      this.angularFS.doc(`Units/${docId}`).update({ unitCost: newCost })
-        .then(() => alert("Success updating electricity unit cost!"))
-        .catch((error) => console.error(error));;
-
-
-    }
-    else if (type == "water") {
+      this.angularFS
+        .doc(`Units/${docId}`)
+        .update({ unitCost: newCost })
+        .then(() => alert('Success updating electricity unit cost!'))
+        .catch((error) => console.error(error));
+    } else if (type == 'water') {
       const docId = 'nwjrpjk3SaF6zfE2WYhr';
-      this.angularFS.doc(`Units/${docId}`).update({ unitCost: newCost })
-        .then(() => alert("Success updating water unit cost!"))
-        .catch((error) => console.error(error));;
-
-    }
-    else {
-      alert("could not update unit cost");
+      this.angularFS
+        .doc(`Units/${docId}`)
+        .update({ unitCost: newCost })
+        .then(() => alert('Success updating water unit cost!'))
+        .catch((error) => console.error(error));
+    } else {
+      alert('could not update unit cost');
     }
   }
-
+  getUnitCostOfWater() {
+    const docRef = this.angularFS.doc(`Units/nwjrpjk3SaF6zfE2WYhr`);
+    docRef.valueChanges().subscribe((docData: any) => {
+      this.unitCostOfWater = docData.unitCost;
+    });
+    return this.unitCostOfWater;
+  }
+  getUnitCostOfElectricity() {
+    const docRef = this.angularFS.doc(`Units/VaTEFMevPbnyWUb8O1XR`);
+    docRef.valueChanges().subscribe((docData: any) => {
+      this.unitCostOfElectricity= docData.unitCost;
+    });
+    return this.unitCostOfElectricity;
+  }
 }
-
-
-
-
-
-
-
--- InvoiceSystem/src/app/update-unit-cost/update-unit-cost.component.ts
+++ InvoiceSystem/src/app/update-unit-cost/update-unit-cost.component.ts
@@ -1,28 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UnitCostService } from 'src/app/services/unit-cost.service';
 
 @Component({
   selector: 'app-update-unit-cost',
   templateUrl: './update-unit-cost.component.html',
-  styleUrls: ['./update-unit-cost.component.css']
+  styleUrls: ['./update-unit-cost.component.css'],
 })
-export class UpdateUnitCostComponent {
-
-  constructor(private UnitCostService : UnitCostService ){}
-  waterUnitCost: string=''; 
-  electricityUnitCost: string=''; 
-
-// todo update unit cost in services
-updateWaterUnitCost()
-{
-  this.UnitCostService.updateUnitCost(parseInt(this.waterUnitCost),"water")
-
-
-}
-updateElectricityUnitCost()
-{
-  this.UnitCostService.updateUnitCost(parseInt(this.electricityUnitCost),"electricity")
-
-}
-
+export class UpdateUnitCostComponent implements OnInit {
+  constructor(private UnitCostService: UnitCostService) {}
+  waterUnitCost: string = '';
+  electricityUnitCost: string = '';
+  curWaterCost: string = '';
+  curElectricityCost: string = '';
+  ngOnInit() {
+    this.waterUnitCost = this.UnitCostService.getUnitCostOfWater();
+    this.electricityUnitCost = this.UnitCostService.getUnitCostOfElectricity();
+    console.log(this.UnitCostService.getUnitCostOfWater(), this.UnitCostService.getUnitCostOfElectricity());
+  }
+  // todo update unit cost in services
+  updateWaterUnitCost() {
+    this.UnitCostService.updateUnitCost(parseInt(this.waterUnitCost), 'water');
+  }
+  updateElectricityUnitCost() {
+    this.UnitCostService.updateUnitCost(
+      parseInt(this.electricityUnitCost),
+      'electricity'
+    );
+  }
 }
