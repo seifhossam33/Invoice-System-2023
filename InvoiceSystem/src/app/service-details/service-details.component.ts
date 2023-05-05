e1c09e4ce17d935f8b10f25c194d462183df8156
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2023-05-05 04:07:57 +0300

    clean code

--- InvoiceSystem/src/app/service-details/service-details.component.ts
+++ InvoiceSystem/src/app/service-details/service-details.component.ts
@@ -1,14 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ServiceOffersService } from '../services/service-offers.service';
 import { ActivatedRoute } from '@angular/router';
-import { OffersType } from '../types/telephoneTypes';
 
 @Component({
   selector: 'app-service-details',
   templateUrl: './service-details.component.html',
   styleUrls: ['./service-details.component.css'],
 })
-export class ServiceDetailsComponent {
+export class ServiceDetailsComponent implements OnInit {
   constructor(
     private service: ServiceOffersService,
     private route: ActivatedRoute
@@ -21,13 +20,14 @@ export class ServiceDetailsComponent {
   offersData: any[] = [];
   ngOnInit() {
     this.route.params.subscribe((params) => {
+      // get the id of the current service from the url
       this.serviceId = params['id'];
     });
 
     this.service.getServiceData(this.serviceId).subscribe((data) => {
       this.offersData = data.serviceOffers;
       this.serviceName = data.serviceName;
-      console.log(this.offersData);
+      //  console.log(this.offersData);
     });
   }
   OnAddNewOfferClick() {
