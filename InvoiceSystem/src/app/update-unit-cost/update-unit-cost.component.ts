c1ab708ee448e98f6db2e74e106b9dc9849d92cd
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2023-05-06 11:27:53 +0300

    remove comments

--- InvoiceSystem/src/app/update-unit-cost/update-unit-cost.component.ts
+++ InvoiceSystem/src/app/update-unit-cost/update-unit-cost.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { UnitCostService } from 'src/app/services/unit-cost.service';
-import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-update-unit-cost',
@@ -13,10 +12,6 @@ export class UpdateUnitCostComponent implements OnInit {
   electricityUnitCost: number = 0;
   isAlertVisibleForWater: boolean = false;
   isAlertVisibleForElectricity: boolean = false;
-  waterUnitCostFormControl = new FormControl('', [
-    Validators.required,
-    Validators.pattern(/^\d+(\.\d{1,2})?$/)
-  ]);
   ngOnInit() {
     this.UnitCostService.getUnitCostOfWater().subscribe((unitCostOfWater) => {
       this.waterUnitCost = unitCostOfWater;
